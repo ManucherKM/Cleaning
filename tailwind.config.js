1869112b074e8b5e36bd44d1b1d963f6b1ff6d35
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-11-03 11:52:43 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "20px"
+    },
+    extend: {
+      colors:{
+        blue: "#0075FF",
+        mgray: "#6E7191"
+      },
+    },
+  },
+  plugins: [],
+}
